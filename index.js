2548453f91f0ce38d83c1deee04c579b87aed8a4
YanGao85 <34007805+YanGao85@users.noreply.github.com>
2021-08-05 18:48:17 -0400

    got error with stretch

--- index.js
+++ index.js
@@ -186,105 +186,105 @@ Use the getRandomFlavors function and new arrays below to do the following:
 */
 
 function getRandomFlavors(arr1, arr2, arr3, arr4) {
-  let newArr = [...arr1, ...arr2, ...arr3, ...arr4];
-  let returnArr = [];
-  while (returnArr.length < 31) {
-    let randomIdx = Math.round(Math.random() * 30);
-    if (!returnArr.includes(newArr[randomIdx]))
-      returnArr.push(newArr[randomIdx]);
-  }
-  return returnArr;
+  // const newArr = [...arr1, ...arr2, ...arr3, ...arr4];
+  // const returnArr = [];
+  // while (returnArr.length < 31) {
+  //   let randomIdx = Math.round(Math.random() * 30);
+  //   if (!returnArr.includes(newArr[randomIdx]))
+  //     returnArr.push(newArr[randomIdx]);
+  // }
+  // return returnArr;
 }
 
-console.log(
-  getRandomFlavors(
-    newFlavors,
-    seasonalFlavors,
-    regionalFlavors,
-    originalFlavors
-  )
-);
+// console.log(
+//   getRandomFlavors(
+//     newFlavors,
+//     seasonalFlavors,
+//     regionalFlavors,
+//     originalFlavors
+//   )
+// );
 
 // NEW DATA ARRAYS FOR STRETCH 2 ⬇️
-const newFlavors = [
-  'Date night',
-  'U.S.S Butterscotch (Stranger Things special)',
-  'Honey Almond',
-  'Mint Chocolate Chip',
-  'Chocolate',
-  "Oreo® Cookies'n Cream",
-  'Chocolate Chip',
-  "Pralines 'n Cream",
-  'Very Berry Strawberry',
-  'Chocolate Chip Cookie Dough',
-  'Old Fashioned Butter Pecan',
-  'Jamoca®',
-  'Jamoca® Almond Fudge',
-  "Reese's® Peanut Butter Cup",
-  'Rocky Road',
-  'Peanut Butter ’n Chocolate',
-  'Gold Medal Ribbon®',
-  'World Class® Chocolate',
-  'Cherries Jubilee',
-  'Chocolate Fudge',
-  'Daiquiri Ice',
-  'Rainbow Sherbet',
-  'Rainbow Swirl',
-];
+// const newFlavors = [
+//   'Date night',
+//   'U.S.S Butterscotch (Stranger Things special)',
+//   'Honey Almond',
+//   'Mint Chocolate Chip',
+//   'Chocolate',
+//   "Oreo® Cookies'n Cream",
+//   'Chocolate Chip',
+//   "Pralines 'n Cream",
+//   'Very Berry Strawberry',
+//   'Chocolate Chip Cookie Dough',
+//   'Old Fashioned Butter Pecan',
+//   'Jamoca®',
+//   'Jamoca® Almond Fudge',
+//   "Reese's® Peanut Butter Cup",
+//   'Rocky Road',
+//   'Peanut Butter ’n Chocolate',
+//   'Gold Medal Ribbon®',
+//   'World Class® Chocolate',
+//   'Cherries Jubilee',
+//   'Chocolate Fudge',
+//   'Daiquiri Ice',
+//   'Rainbow Sherbet',
+//   'Rainbow Swirl',
+// ];
 
-const seasonalFlavors = [
-  "America's Birthday Cake",
-  'Baseball Nut®',
-  'Blueberry Cheesecake',
-  'Bourbon Street Pecan Pie',
-  'Brownie Bar Mashup',
-  'Cherry Cordial with Kisses',
-  'Chocolate Mousse Royale',
-  'French Vanilla',
-  'Eggnog',
-  'German Chocolate Cake',
-  'Icing on the Cake',
-  'Love Potion #31',
-  'New York Cheesecake',
-  'Nutty Coconut',
-  'Peppermint',
-  'Strawberry Cheesecake',
-  'Rock ’n Pop Swirl',
-  'Reese’s Peanut Butter Cup',
-  'Trick Oreo Treat',
-  'Winter White Chocolate',
-  'made with Snickers®',
-  "made with M&M's®",
-  'Heath®',
-  'Mango Tango',
-];
+// const seasonalFlavors = [
+//   "America's Birthday Cake",
+//   'Baseball Nut®',
+//   'Blueberry Cheesecake',
+//   'Bourbon Street Pecan Pie',
+//   'Brownie Bar Mashup',
+//   'Cherry Cordial with Kisses',
+//   'Chocolate Mousse Royale',
+//   'French Vanilla',
+//   'Eggnog',
+//   'German Chocolate Cake',
+//   'Icing on the Cake',
+//   'Love Potion #31',
+//   'New York Cheesecake',
+//   'Nutty Coconut',
+//   'Peppermint',
+//   'Strawberry Cheesecake',
+//   'Rock ’n Pop Swirl',
+//   'Reese’s Peanut Butter Cup',
+//   'Trick Oreo Treat',
+//   'Winter White Chocolate',
+//   'made with Snickers®',
+//   "made with M&M's®",
+//   'Heath®',
+//   'Mango Tango',
+// ];
 
-const regionalFlavors = [
-  'Pink Bubblegum',
-  'Caramel Macchiato',
-  'York Peppermint Pattie',
-  'Cotton Candy',
-  'Orange Sherbet',
-  'Grape Ice',
-  'Watermelon Ice',
-  'Miami Vice Sorbet',
-  'Splish Splash®',
-  "Wild 'n Reckless Sherbet",
-  'Lemon Custard',
-  'Oregon Blackberry',
-  'Bananas ‘n Strawberries',
-  'Mississippi Mud',
-  'Rum Raisin',
-  'Creole Cream Cheese',
-  'Chocolate Almond',
-  'Fudge Brownie',
-  'Banana Nut',
-  'Black Walnut',
-  'Cotton Candy Crackle',
-  'Quarterback Crunch',
-  'Chocolate Chocolate Chip Cheesecake',
-  "Caramel 'n' Cookies",
-];
+// const regionalFlavors = [
+//   'Pink Bubblegum',
+//   'Caramel Macchiato',
+//   'York Peppermint Pattie',
+//   'Cotton Candy',
+//   'Orange Sherbet',
+//   'Grape Ice',
+//   'Watermelon Ice',
+//   'Miami Vice Sorbet',
+//   'Splish Splash®',
+//   "Wild 'n Reckless Sherbet",
+//   'Lemon Custard',
+//   'Oregon Blackberry',
+//   'Bananas ‘n Strawberries',
+//   'Mississippi Mud',
+//   'Rum Raisin',
+//   'Creole Cream Cheese',
+//   'Chocolate Almond',
+//   'Fudge Brownie',
+//   'Banana Nut',
+//   'Black Walnut',
+//   'Cotton Candy Crackle',
+//   'Quarterback Crunch',
+//   'Chocolate Chocolate Chip Cheesecake',
+//   "Caramel 'n' Cookies",
+// ];
 
 /* 🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑 Please do not modify anything below this line 🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑 */
 function foo() {
